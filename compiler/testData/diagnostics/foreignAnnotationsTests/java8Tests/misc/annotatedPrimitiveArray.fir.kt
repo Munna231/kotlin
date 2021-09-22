647b11d58b6e33ef3a4120a4127f7660d3312c68
pyos <pyos@google.com>
2021-09-22 13:14:05 +0200

    FE: don't lose annotations on Java primitive arrays
    but only use them to enhance for warnings for now.
    
    ^KT-48861 Fixed

---
+++ compiler/testData/diagnostics/foreignAnnotationsTests/java8Tests/misc/annotatedPrimitiveArray.fir.kt
@@ -0,0 +1,10 @@
+// SKIP_TXT
+// FILE: J.java
+import org.jetbrains.annotations.Nullable;
+
+public interface J {
+    int @Nullable [] foo();
+}
+
+// FILE: main.kt
+fun bar(j: J) = j.foo()<!UNSAFE_CALL!>.<!>iterator()
---
+++ compiler/testData/diagnostics/foreignAnnotationsTests/java8Tests/misc/annotatedPrimitiveArray.kt
@@ -0,0 +1,10 @@
+// SKIP_TXT
+// FILE: J.java
+import org.jetbrains.annotations.Nullable;
+
+public interface J {
+    int @Nullable [] foo();
+}
+
+// FILE: main.kt
+fun bar(j: J) = <!RECEIVER_NULLABILITY_MISMATCH_BASED_ON_JAVA_ANNOTATIONS!>j.foo()<!>.iterator()

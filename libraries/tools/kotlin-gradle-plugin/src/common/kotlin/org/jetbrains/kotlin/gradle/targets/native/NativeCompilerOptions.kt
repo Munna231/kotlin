ce05a3b9f17a0048eeb2fa78c8b04dfaefd01eab
Sebastian Sellmair <sebastian.sellmair@jetbrains.com>
2022-09-30 00:07:08 +0200

    [Gradle] Implement Kotlin*NativeCompilations with underlying KotlinCompilationImpl
    KT-54312

--- libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/targets/native/NativeCompilerOptions.kt
+++ libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/targets/native/NativeCompilerOptions.kt
@@ -6,23 +6,49 @@
 package org.jetbrains.kotlin.gradle.targets.native
 
 import org.gradle.api.Project
-import org.jetbrains.kotlin.gradle.dsl.KotlinCommonCompilerOptions
-import org.jetbrains.kotlin.gradle.dsl.KotlinCommonCompilerOptionsDefault
+import org.jetbrains.kotlin.gradle.dsl.CompilerCommonOptions
+import org.jetbrains.kotlin.gradle.dsl.CompilerCommonOptionsDefault
 import org.jetbrains.kotlin.gradle.plugin.HasCompilerOptions
 import org.jetbrains.kotlin.gradle.plugin.runOnceAfterEvaluated
 import org.jetbrains.kotlin.gradle.plugin.sources.applyLanguageSettingsToCompilerOptions
 import org.jetbrains.kotlin.project.model.LanguageSettings
+import org.jetbrains.kotlin.tooling.core.UnsafeApi
+import java.util.*
 
-internal class NativeCompilerOptions(
+internal class NativeCompilerOptions
+/**
+ * Unsafe constructor: Not providing languageSettings requires to call 'applyLanguageSettingsToCompilerOptions'  manually!
+ */
+@UnsafeApi constructor(
     project: Project,
-    nativeLanguageSettings: LanguageSettings
-) : HasCompilerOptions<KotlinCommonCompilerOptions> {
-    override val options: KotlinCommonCompilerOptions = project.objects
-        .newInstance(KotlinCommonCompilerOptionsDefault::class.java)
+    languageSettings: Optional<LanguageSettings>
+) : HasCompilerOptions<CompilerCommonOptions> {
+
+    @OptIn(UnsafeApi::class)
+    constructor(project: Project, languageSettings: LanguageSettings) : this(
+        project, Optional.of(languageSettings)
+    )
+
+    override val options: CompilerCommonOptions = project.objects
+        .newInstance(CompilerCommonOptionsDefault::class.java)
         .apply {
             useK2.finalizeValue()
-            project.runOnceAfterEvaluated("apply Kotlin native properties from language settings") {
-                applyLanguageSettingsToCompilerOptions(nativeLanguageSettings, this)
+
+            if (languageSettings.isPresent) {
+                @OptIn(UnsafeApi::class)
+                applyLanguageSettingsToCompilerOptions(project, languageSettings.get(), this)
             }
         }
+
+    companion object {
+        /**
+         * Only call when no languageSettings were provided in the constructor!
+         */
+        @UnsafeApi
+        fun applyLanguageSettingsToCompilerOptions(project: Project, languageSettings: LanguageSettings, options: CompilerCommonOptions) {
+            project.runOnceAfterEvaluated("apply Kotlin native properties from language settings") {
+                applyLanguageSettingsToCompilerOptions(languageSettings, options)
+            }
+        }
+    }
 }
